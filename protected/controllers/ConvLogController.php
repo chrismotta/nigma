cb2baeca30c4477efd3cbf89cbc5b19773f18a81
Christian <christian.motta@kickads.mobi>
2014-06-17 15:18:11 -0300

    testeo de google conversion tracking generando un log

--- protected/controllers/ConvLogController.php
+++ protected/controllers/ConvLogController.php
@@ -4,6 +4,40 @@ class ConvLogController extends Controller
 {
 	public function actionIndex()
 	{
+		/*
+
+		$gc_callback = "http://www.googleadservices.com/pagead/";
+		$gc_callback.= "conversion/970651684/";
+		$gc_callback.= "?label=wAWJCLyWhQoQpPDrzgM";
+		$gc_callback.= "&guid=ON&script=0";
+
+		$gc_callback = "http://localhost/kickads/appserver/conv.php?mytoken=".$_GET['mytoken'];
+		//print $gc_callback ."<hr/>";
+		// Crea un nuevo recurso cURL
+		$curl = curl_init();
+
+		// Establece la URL y otras opciones apropiadas
+		curl_setopt($curl, CURLOPT_URL, $gc_callback);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		// Captura la URL y la envía al navegador
+		$exec = curl_exec($curl);
+
+		// Cierrar el recurso cURLy libera recursos del sistema
+		curl_close($curl);
+
+		//echo "<img src='//".$exec."' />";
+
+		$gc_log = fopen("gc.log", "a");
+		fwrite($gc_log, $_GET['mytoken']."\n\r");
+		fclose($gc_log);
+		
+		print "<hr/>".$exec;
+
+		Yii::app()->end();
+
+		*/
 
 		// Get Request
 		if( isset( $_GET['mytoken'] ) ){
@@ -66,6 +100,11 @@ class ConvLogController extends Controller
 
 						// Cierrar el recurso cURLy libera recursos del sistema
 						curl_close($curl);
+
+						$gc_log = fopen("gc.log", "a");
+						fwrite($gc_log, $_GET['mytoken']."\n\r");
+						fclose($gc_log);
+						
 					}
 				}
 
@@ -76,54 +115,6 @@ class ConvLogController extends Controller
 		}
 
 		Yii::app()->end();
-
-
-
-
-
-		//print_r($campaign);
-		//print "url: ".$redirectURL."<hr/>";
-
-		// Write down a log
-
-		$model = new ClicksLog();
-		//$model->id = 2;
-		$model->campaigns_id = (int)$cid;
-		$model->networks_id = (int)$nid;
-		//$model->date = 0;
-
-		// Get visitor parameters
-		
-		$model->server_ip = isset($_SERVER["SERVER_ADDR"]) ? $_SERVER["SERVER_ADDR"] : null;
-		//"$model->forwarded_ip = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : null;
-		$model->server_name = isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : null;
-		$model->user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
-		$model->languaje = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : null;
-		$model->referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
-
-		//var_dump($model);
-		//print "<hr/>";
-
-		// Save active record and redirect
-		
-		if($model->save()){
-
-			$mytoken = md5($model->id);
-			//print "guardado - tid: ".$mytoken;
-			//print "<hr/>";
-			$model->tid = $mytoken;
-			$model->save();
-
-			// Guardo los datos en cookies (Expira en 1 hora)
-			setcookie('sma_tid', $mytoken, time() + 1 * 1 * 60 * 60, '/');
-
-			$redirectURL.= "&mytoken=".$mytoken;
-			//print $redirectURL;
-			// redirect to campaign url
-			//$this->redirect($redirectURL);
-		}else{
-			//print "no guardado";
-		}
 
 	}
 
