c2111498eecff987f4c0a32e8515da2ffd17bc84
Matias <matias@matias-Inspiron-3420.(none)>
2014-07-17 15:22:55 -0300

    Agrego controller para Airpush API.

---
+++ protected/controllers/api/AirpushController.php
@@ -0,0 +1,74 @@
+<?php
+
+class AirpushController extends Controller
+{
+
+	private $network_id = 1;
+
+	public function actionIndex()
+	{
+
+		if ( isset( $_GET['date']) ) {
+			$date = $_GET['date'];
+		} else {
+			// print "Parameter 'date' missing.";
+			Yii::app()->end(1);
+		}
+
+		// validate if info have't been dowloades already.
+		if ( DailyReport::model()->exists("networks_id=:network AND DATE(date)=:date", array(":network"=>$this->network_id, ":date"=>$date)) ) {
+			// print "Information already downloaded.";
+			Yii::app()->end(2);
+		}
+
+		// Get json from Airpush API.
+		$network = Networks::model()->findbyPk($this->network_id);
+		$apikey = $network->token1;
+		$apiurl = $network->url;
+		$url = $apiurl . "?apikey=" . $apikey . "&startDate=" . $date . "&endDate=" . $date;
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($curl);
+		$result = json_decode($result);
+		if (!$result) {
+			// print "ERROR decoding Airpush json";
+			Yii::app()->end(1);
+		}
+		curl_close($curl);
+		
+		// Save campaigns information 
+		foreach ($result->advertiser_data as $campaign) {
+			$dailyReport = new DailyReport();
+			
+			//FIXME el ID de la campaña viene encerrado entre ** en el nombre. Paresarlo directamente.
+			$tmp = Campaigns::model()->findByAttributes( array('name'=>$campaign->campaignname) );
+			if (!$tmp) { // TODO add more validations?
+				print "ERROR - campaign not found: " . $campaign->campaignname . "<br>";
+				continue;
+			}
+
+			// FIXME unncomment when campaign name have the new format's name.
+			// $id_begin = strpos($campaign->campaignname, "*");
+			// $id_end = strpos($campaign->campaignname, "*", $id_begin + 1);
+			// $dailyReport->campaigns_id = substr($campaign->campaignname, $id_begin, $id_end);
+			$dailyReport->campaigns_id = $tmp->id;
+			$dailyReport->networks_id = $this->network_id;
+			$dailyReport->imp = $campaign->impression;
+			$dailyReport->clics = $campaign->clicks;
+			$dailyReport->conv = ConvLog::model()->count("campaign_id=:campaignid AND DATE(date)=:date", array(":campaignid"=>$dailyReport->campaigns_id, ":date"=>$date));
+			$dailyReport->spend = $campaign->Spent;
+			$dailyReport->model = 0;
+			$dailyReport->value = 0;
+			$dailyReport->date = $date;
+			if ( !$dailyReport->save() ) {
+				// print "ERROR - saving campaign: " . $campaign->campaignname . "<br>";
+				continue;
+			}
+			// print "OK - campaign found: " . $campaign->campaignname . ", campaign_id: " . $dailyReport->campaigns_id . ", impressions: " . $dailyReport->imp . ", clicks: " . $dailyReport->clics . ", conv: " . $dailyReport->conv . ", date: " . $dailyReport->date ."<br>";
+		}
+
+		Yii::app()->end();
+	}
+
+}
