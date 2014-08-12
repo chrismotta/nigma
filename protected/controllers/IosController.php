f7df40b54bad3a2688bb61c704d701c185179bf4
Christian <christian.motta@kickads.mobi>
2014-08-12 17:18:16 -0300

    filtro por pais

--- protected/controllers/IosController.php
+++ protected/controllers/IosController.php
@@ -195,7 +195,7 @@ class IosController extends Controller
 		$currency   = KHtml::enumItem($model, 'currency');
 		$entity     = KHtml::enumItem($model, 'entity');
 		$advertiser = CHtml::listData(Advertisers::model()->findAll(), 'id', 'name'); 
-		$country = CHtml::listData(GeoLocation::model()->findAll( "status='Active'" ), 'id_location', 'name' );
+		$country = CHtml::listData(GeoLocation::model()->findAll( array('order'=>'name', "condition"=>"status='Active' AND type='Country'") ), 'id_location', 'name' );
 
 		if ( $model->isNewRecord ) {
 			$model->commercial_id = Yii::app()->user->id;
