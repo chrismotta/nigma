429c34674f8d637f335ac398d19bb82bee734cf5
Christian <christian.motta@kickads.mobi>
2014-08-15 15:53:11 -0300

    import eexcelwriter

--- protected/config/main.php
+++ protected/config/main.php
@@ -62,6 +62,7 @@ return array(
 		'application.external.ip2location.IP2Location',
 		'application.external.wurfl.WurflManager',
 		'ext.pdffactory.*',
+		'ext.eexcelwriter.components.EExcelWriter',
 	),
 
 	'modules'=>array(
@@ -100,12 +101,14 @@ return array(
 			'urlFormat'=>'path',
 			'showScriptName'=>false,
 			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'<controller:\w+>/<id:\d+>'              =>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' =>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'          =>'<controller>/<action>',
 			),
 		),
-
+		'eexcelwriter'=>array(
+            'class'=>'eexcelwriter.components.EExcelWriter',
+        ),
 		'pdfFactory'=>array(
             'class'=>'ext.pdffactory.EPdfFactory',
  
