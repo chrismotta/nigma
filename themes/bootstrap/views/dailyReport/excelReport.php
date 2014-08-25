af1e1e57c2ff72ce6a613a132ea3c2206bacb0b8
mcerrotta <matias.cerrotta@gmail.com>
2014-08-25 11:29:44 -0300

    Agrego exportacion a excel para DailyReport

--- themes/bootstrap/views/dailyReport/excelReport.php
+++ themes/bootstrap/views/dailyReport/excelReport.php
@@ -21,11 +21,39 @@ $this->widget('EExcelWriter', array(
         	'value' => '$data->networks->name',
         ),
         'imp',
+        'imp_adv',
         'conv_api',
         'conv_adv',
         'spend',
-        'model',
-        'value',
+        'revenue',
+        array(
+            'name'  => 'profit',
+            'value' =>  '$data->profit',
+        ),
+        array(
+            'name'  => 'click_rate',
+            'value' => '$data->click_rate * 100',
+        ),
+        array(
+            'name'  => 'conv_rate',
+            'value' => '$data->conv_rate * 100',
+        ),
+        array(
+            'name'  => 'profit_perc',
+            'value' => '$data->profit_perc * 100',
+        ),
+        array(
+            'name'  => 'eCPM',
+            'value' => '$data->eCPM',
+        ),
+        array(
+            'name'  => 'eCPC',
+            'value' => '$data->eCPC',
+        ),
+        array(
+            'name'  => 'eCPA',
+            'value' => '$data->eCPA',
+        ),
         array(
             'name' => 'date',
             'value' => 'date("d-m-Y", strtotime($data->date))',
