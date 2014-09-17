abefbdef79bee413b06c0f4adcbffe12a8c2b6f5
Santiago <santiagomenape@gmail.com>
2014-09-17 18:29:46 -0300

    Ero

--- protected/models/api/EroAdvertising.php
+++ protected/models/api/EroAdvertising.php
@@ -25,8 +25,7 @@ class EroAdvertising
 		$apiurl = $network->url;
 		$url = $apiurl . "?token=" . $apikey . "&periodstart=" . $date . "&periodend=" . $date;
 		//https://userpanel.ero-advertising.com/exportstats/advertiser/stats/?token=166dcc6c27d05e39024f034db0d7fd03&periodstart=yyyy-mm-dd&periodend=yyyy-mm-dd
-		echo $url;
-		return;
+		
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		$result = curl_exec($curl);
