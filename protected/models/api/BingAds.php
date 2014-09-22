ee1713ed17a649a948b198694bec9ec971ad8d4e
mcerrotta <matias.cerrotta@gmail.com>
2014-09-22 16:46:18 -0300

    Agrego API BingAds

---
+++ protected/models/api/BingAds.php
@@ -0,0 +1,43 @@
+<?php
+
+class BingAds
+{ 
+	private $network_id = 20;
+
+	public function downloadInfo()
+	{
+		echo "ERROR - Not implemented. <br>";
+
+		// // Config values
+		// $wsdl = "https://api.bingads.microsoft.com/Api/Advertiser/Reporting/V9/ReportingService.svc?singleWsdl";
+		// $UserName = "<UserNameGoesHere>";
+		// $Password = "<PasswordGoesHere>";
+		// $DeveloperToken = "<DeveloperTokenGoesHere>";
+		// $AccountId = <AccountIdGoesHere>;
+		// $CampaignId = <CampaignIdGoesHere>;
+
+
+		// $proxy = ClientProxy::ConstructWithAccountId($wsdl, $UserName, $Password, $DeveloperToken, $AccountId, null);
+	
+	 //    $report->Aggregation = ReportAggregation::Daily;
+
+		// $report->Time = new ReportTime();
+		// if ( isset( $_GET['date']) ) {
+		// 	$report->Time->CustomDateRangeStart = '';
+		// 	$report->Time->CustomDateRangeEnd   = '';
+		// } else {
+		// 	$report->Time->PredefinedTime       = ReportTimePeriod::Yesterday;
+		// }
+
+
+	 //    $report = new CampaignPerformanceReportRequest();
+	 //    $report->Columns = array(
+	 //    	CampaignPerformanceReportRequest::CampaignName,
+	 //    	CampaignPerformanceReportRequest::Impressions,
+	 //    	CampaignPerformanceReportRequest::Clicks,
+	 //    	CampaignPerformanceReportRequest::Spend,
+	 //    	);
+	}
+}
+
+?>
