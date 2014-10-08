7e5ef4006a2dd4e21d6263d35ff7aa505f33b945
Christian <christian.motta@kickads.mobi>
2014-10-08 10:47:12 -0300

    editable field en daily report

--- themes/bootstrap/views/dailyReport/admin.php
+++ themes/bootstrap/views/dailyReport/admin.php
@@ -267,93 +267,71 @@ $('.search-form form').submit(function(){
 	'rowCssClassExpression'    => '$data->getCapStatus() ? "errorCap" : null',
 	'columns'                  => array(
 		array(
-			'name'  =>	'id',
-        	'headerHtmlOptions' => array('style' => 'width: 30px'),
-        	'htmlOptions'	=> array( 'class' =>  'id'),
-        	'footer' => 'Totals:'
+			'name'              =>	'id',
+			'headerHtmlOptions' => array('style' => 'width: 30px'),
+			'footer'            => 'Totals:',
 		),
 		array(
-			'name'  => 'campaign_name',
-			'value' => 'Campaigns::model()->getExternalName($data->campaigns_id)',
+			'name'        => 'campaign_name',
+			'value'       => 'Campaigns::model()->getExternalName($data->campaigns_id)',
 			'htmlOptions' => array('style' => 'width: 120px'),
 		),
 		array(
-			'name'  =>	'network_name',
-			'value'	=>	'$data->networks->name',
+			'name'   =>	'network_name',
+			'value'  =>	'$data->networks->name',
 			'filter' => $networks_names,
 		),
 		array(
-			'name'  => 'rate',
-			'value' => '$data->getRateUSD() ? $data->getRateUSD() : 0',
-			'htmlOptions'=>array('style'=>'width: 45px; text-align:right;'),
+			'name'        => 'rate',
+			'value'       => '$data->getRateUSD() ? $data->getRateUSD() : 0',
+			'htmlOptions' => array('style'=>'width: 45px; text-align:right;'),
 		),
 		array(	
-			'name'	=>	'imp',
-			'htmlOptions'=>array('style'=>'width: 50px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['imp'],
+			'name'              => 'imp',
+			'htmlOptions'       => array('style'=>'width: 50px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['imp'],
         ),
         array(	
-			'name'	=>	'imp_adv',
-			'type'	=>	'raw',
-			'htmlOptions'=>array('style'=>'width: 85px'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-        	'value' =>	'
-					CHtml::textField("row-imp" . $row, $data->imp_adv, array(
-        				"style" => "width:30px; text-align:right; font-size: 11px;", 
-        				"onkeydown" => "
-							var r = $( \"#row-imp\" + $row ).parents( \"tr\" );
-	        				r.removeClass( \"control-group success\" );
-	        				r.addClass( \"control-group error\" );
-        				" 
-        				)) . " " .
-        			CHtml::ajaxLink(
-            				"<i class=\"icon-pencil\"></i>",
-	            			Yii::app()->controller->createUrl("updateColumn"),
-	        				array(
-								"type"     => "POST",
-								"dataType" => "json",
-								"data"     => array( "id" => "js:$.fn.yiiGridView.getKey(\"daily-report-grid\", $row)",	 "newValue" => "js:$(\"#row-imp\" + $row).val()", "col" => "imp_adv" ) ,
-								"success"  => "function( data )
-									{
-										$.fn.yiiGridView.update(\"daily-report-grid\", {
-											complete: function(jqXHR, textStatus) {
-												if (textStatus == \'success\') {
-													// change css properties
-													var r = $( \"#row-imp\" + $row ).parents( \"tr\" );
-													r.removeClass( \"control-group error\" );
-													r.addClass( \"control-group success\" );
-												}
-											}
-										});
-									}",
-								),
-							array(
-								"style"               => "width: 20px",
-								"rel"                 => "tooltip",
-								"data-original-title" => "Update"
-								)
-						)
-        	',
-			'footer'=>$totals['imp_adv'],
+			'name'              => 'imp_adv',
+			'type'              => 'raw',
+			'htmlOptions'       => array('style'=>'width: 50px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['imp_adv'],
+			'class'             => 'bootstrap.widgets.TbEditableColumn',
+			'editable'          => array(
+				'title'     => 'Comment',
+				'type'      => 'text',
+				'url'       => 'updateEditable/',
+				'name'      => 'comment',
+				'emptytext' => 'Null',
+            ),
         ),
         array(
-        	'name'	=>	'clics',
-        	'htmlOptions'=>array('style'=>'width: 50px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['clics'],
+			'name'              => 'clics',
+			'htmlOptions'       => array('style'=>'width: 50px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['clics'],
         ),
         array(
-        	'name'	=>	'conv_api',
-        	'htmlOptions'=>array('style'=>'width: 50px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['conv_s2s'],
+			'name'              => 'conv_api',
+			'htmlOptions'       => array('style'=>'width: 50px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['conv_s2s'],
         ),
 		array(
-			'name'        => 'conv_adv',
+			'name'              => 'conv_adv',
+			'htmlOptions'       => array('style'=>'width: 85px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			/*'class'             => 'bootstrap.widgets.TbEditableColumn',
+            'editable' => array(
+				'title'     => 'Comment',
+				'type'      => 'text',
+				'url'       => 'updateEditable/',
+				'name'      => 'comment',
+				'emptytext' => 'Null',
+            ),*/
 			'type'        => 'raw',
-			'htmlOptions' => array('style'=>'width: 85px'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
 			'value'       =>	'
         			CHtml::textField("row-conv" . $row, $data->conv_adv, array(
         				"style" => "width:30px; text-align:right; font-size: 11px;",
@@ -429,63 +407,63 @@ $('.search-form form').submit(function(){
 			'footer'=>$totals['conv_adv'],
         ),
         array(
-        	'name' => 'revenue',
-        	'value' => '$data->getRevenueUSD()',
-        	'htmlOptions'=>array('style'=>'width: 70px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['revenue'],
+			'name'              => 'revenue',
+			'value'             => '$data->getRevenueUSD()',
+			'htmlOptions'       => array('style'=>'width: 70px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['revenue'],
         ),
 		array(
-        	'name'	=>	'spend',
-        	'value'	=>	'$data->getSpendUSD()',
-        	'htmlOptions'=>array('style'=>'width: 60px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['spend'],
+			'name'              => 'spend',
+			'value'             => '$data->getSpendUSD()',
+			'htmlOptions'       => array('style'=>'width: 60px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['spend'],
         ),
 		array(
-			'name'  => 'profit',
-			'htmlOptions'=>array('style'=>'width: 60px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['profit'],
+			'name'              => 'profit',
+			'htmlOptions'       => array('style'=>'width: 60px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['profit'],
 		),
 		array(
-			'name'  => 'profit_percent',
-			'value' => '$data->profit_percent * 100 . "%"',
-			'htmlOptions'=>array('style'=>'width: 30px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>($totals['profitperc']*100)."%",
+			'name'              => 'profit_percent',
+			'value'             => '$data->profit_percent * 100 . "%"',
+			'htmlOptions'       => array('style'=>'width: 30px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => ($totals['profitperc']*100)."%",
 		),
 		array(
-			'name'  => 'click_through_rate',
-			'value' => '$data->click_through_rate * 100 . "%"',
-			'htmlOptions'=>array('style'=>'width: 30px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>($totals['ctr']*100)."%",
+			'name'              => 'click_through_rate',
+			'value'             => '$data->click_through_rate * 100 . "%"',
+			'htmlOptions'       => array('style'=>'width: 30px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => ($totals['ctr']*100)."%",
 		),
 		array(
-			'name'  => 'conversion_rate',
-			'value' => '$data->conversion_rate * 100 . "%"',
-			'htmlOptions'=>array('style'=>'width: 30px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>($totals['cr']*100)."%",
+			'name'              => 'conversion_rate',
+			'value'             => '$data->conversion_rate * 100 . "%"',
+			'htmlOptions'       => array('style'=>'width: 30px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => ($totals['cr']*100)."%",
 		),
 		array(
-			'name'  => 'eCPM',
-			'htmlOptions'=>array('style'=>'width: 45px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['ecpm'],
+			'name'              => 'eCPM',
+			'htmlOptions'       => array('style'=>'width: 45px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['ecpm'],
 		),
 		array(
-			'name'  => 'eCPC',
-			'htmlOptions'=>array('style'=>'width: 45px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['ecpc'],
+			'name'              => 'eCPC',
+			'htmlOptions'       => array('style'=>'width: 45px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['ecpc'],
 		),
 		array(
-			'name'  => 'eCPA',
-			'htmlOptions'=>array('style'=>'width: 45px; text-align:right;'),
-			'footerHtmlOptions'=>array('style'=>'text-align:right;'),
-			'footer'=>$totals['ecpa'],
+			'name'              => 'eCPA',
+			'htmlOptions'       => array('style'=>'width: 45px; text-align:right;'),
+			'footerHtmlOptions' => array('style'=>'text-align:right;'),
+			'footer'            => $totals['ecpa'],
 		),
 		array(
 			'name'        => 'date',
