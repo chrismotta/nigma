b28c9e39679a1fe74f47a374c46feff3d77d2ec7
Santiago <santiagomenape@gmail.com>
2014-10-10 13:23:48 -0300

    clicks_redirect add

--- themes/bootstrap/views/campaigns/traffic.php
+++ themes/bootstrap/views/campaigns/traffic.php
@@ -51,7 +51,7 @@ Yii::app()->clientScript->registerScript('search', "
 				'title' => array('text' => '')
 				),
 			'series' => array(
-				array('name' => 'Clicks', 'data' => $totalsGrap['clics'],),
+				array('name' => 'Clicks', 'data' => $totalsGrap['clics_redirect'],),
 				array('name' => 'Conversions', 'data' => $totalsGrap['conversions'],),
 				),
 	        'legend' => array(
@@ -232,7 +232,7 @@ Yii::app()->clientScript->registerScript('search', "
 			'htmlOptions'		=> array('style'=>'width: 45px; text-align:right;'),
 			'filter'			=> '',
 			'footerHtmlOptions'	=>array('style'=>'text-align:right;'),
-			'footer'			=> array_sum($totals["clics"]),
+			'footer'			=> array_sum($totals["clics_redirect"]),
         ),
         array(
 			'name'              => 'conv',
