7ff9ffe29c3314f088f32148e4a35ab184121727
Christian <christian.motta@kickads.mobi>
2015-01-08 12:11:34 -0300

    mail external form

--- protected/controllers/MailTestController.php
+++ protected/controllers/MailTestController.php
@@ -22,40 +22,29 @@ class MailTestController extends Controller
 		Yii::log('Log - level: mail', 'mail', 'system.mail');
 
 		$body = '
-Dear client:
-
-Please check the statement of your account by following the link below. We will assume that you are in agreement with us on the statement unless you inform us to the contrary by latest (+ 4 DAYS)
-
-LINK
-
-If you weren’t the right contact person to verify the invoice, we ask you to follow the link above and update the information. Do not reply to this email with additional information.
-
-This process allows us to audit the invoice together beforehand and expedite any paperwork required and payment.
-
-Thanks
-
-KickAds
-
-<hr/>
+<p>Dear client:</p>
+<p>Please check the statement of your account by following the link below. We will assume that you are in agreement with us on the statement unless you inform us to the contrary by latest (+ 4 DAYS)</p>
+<p>LINK</p>
+<p>If you weren’t the right contact person to verify the invoice, we ask you to follow the link above and update the information. Do not reply to this email with additional information.</p>
+<p>This process allows us to audit the invoice together beforehand and expedite any paperwork required and payment.</p>
+<p>Thanks</p>
+<p>KickAds</p>
+<br/>
 <span style="color:#555">
-Estimado cliente:
-
-Por favor verificar el estado de su cuenta a través del link a continuación. Se considerara de acuerdo con el estado actual a menos que se nos notifique lo contrario a mas tardar el (+ 4 DAYS)
-
-LINK
-
-Si usted no fuese la persona indicada para hacer esta verificación, le solicitamos ingrese al link anterior y actualice los datos. No responda a este correo con información adicional.
-
-Este proceso nos permite auditar en conjunto la facturación previo a realizar y agilizar en lo posible el intercambio de documentos y el pago.
-
-Gracias 
-
-KickAds
+<p>Estimado cliente:</p>
+<p>Por favor verificar el estado de su cuenta a través del link a continuación. Se considerara de acuerdo con el estado actual a menos que se nos notifique lo contrario a mas tardar el (+ 4 DAYS)</p>
+<p>LINK</p>
+<p>Si usted no fuese la persona indicada para hacer esta verificación, le solicitamos ingrese al link anterior y actualice los datos. No responda a este correo con información adicional.</p>
+<p>Este proceso nos permite auditar en conjunto la facturación previo a realizar y agilizar en lo posible el intercambio de documentos y el pago.</p>
+<p>Gracias</p> 
+<p>KickAds</p>
 </span>
 		';
 
 		$mail = new CPhpMailerLogRoute;
 		$mail->send(array('christian.motta@kickads.mobi'), 'Custom Mail', $body);
+
+		echo 'Enviado '.date('H:m:s');
 	}
 }
 
--- themes/bootstrap/views/finance/sendMail.php
+++ themes/bootstrap/views/finance/sendMail.php
@@ -21,7 +21,7 @@ $log               = new ValidationLog;
 			$body = '
 					<span style="color:#000">
 					  <p>Dear client:</p>
-					  <p>Please check the statement of your account by following the link below. We will assume that you are in agreement with us on the statement unless you inform us to the contrary by latest '.date('M j, Y').'</p>
+					  <p>Please check the statement of your account by following the link below. We will assume that you are in agreement with us on the statement unless you inform us to the contrary by latest '.date('M j, Y', strtotime('+4 days')).'</p>
 					  <p><a href="http://kickadserver.mobi/externalForms/revenueValidation/'.$ioValidation->validation_token.'">http://kickadserver.mobi/externalForms/revenueValidation/'.$ioValidation->validation_token.'</a></p>
 					  <p>If you weren’t the right contact person to verify the invoice, we ask you to follow the link above and update the information. Do not reply to this email with additional information.</p>
 					  <p>This process allows us to audit the invoice together beforehand and expedite any paperwork required and payment.</p>
@@ -30,7 +30,7 @@ $log               = new ValidationLog;
 					<hr style="border: none; border-bottom: 1px solid #999;"/>
 					<span style="color:#666">
 					  <p>Estimado cliente:</p>
-					  <p>Por favor verificar el estado de su cuenta a través del link a continuación. Se considerara de acuerdo con el estado actual a menos que se nos notifique lo contrario a mas tardar el '.date('d-m-Y').'</p>
+					  <p>Por favor verificar el estado de su cuenta a través del link a continuación. Se considerara de acuerdo con el estado actual a menos que se nos notifique lo contrario a mas tardar el '.date('d-m-Y', strtotime('+4 days')).'</p>
 					  <p><a href="http://kickadserver.mobi/externalForms/revenueValidation/'.$ioValidation->validation_token.'">http://kickadserver.mobi/externalForms/revenueValidation/'.$ioValidation->validation_token.'</a></p>
 					  <p>Si usted no fuese la persona indicada para hacer esta verificación, le solicitamos ingrese al link anterior y actualice los datos. No responda a este correo con información adicional.</p>
 					  <p>Este proceso nos permite auditar en conjunto la facturación previo a realizar y agilizar en lo posible el intercambio de documentos y el pago.</p>
