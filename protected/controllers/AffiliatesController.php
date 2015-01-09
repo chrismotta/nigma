059bf588b9bcd4cb1b047bca626c010a6b870191
mcerrotta <matias.cerrotta@gmail.com>
2015-01-09 11:22:12 -0300

    Agrego filters a AffiliatesController

--- protected/controllers/AffiliatesController.php
+++ protected/controllers/AffiliatesController.php
@@ -2,6 +2,23 @@
 
 class AffiliatesController extends Controller
 {
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column1', meaning
+	 * using two-column layout. See 'protected/views/layouts/column1.php'.
+	 */
+	public $layout='//layouts/column1';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+	
 	public function accessRules()
 	{
 		return array(
