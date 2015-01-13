410de6ac8c9d6d5792c3f36c619aa3fc40d7b521
Santiago <santiagomenape@gmail.com>
2015-01-13 15:26:53 -0300

    Delete button transactions

--- themes/bootstrap/views/finance/_form.php
+++ themes/bootstrap/views/finance/_form.php
@@ -75,10 +75,34 @@
                 array('name'              =>'rate'),
                 array('name'              =>'users_id', 'value'=>'$data->getUserName()'),
                 array('name'              =>'date'),
-                array(
-					    'class'=>'CButtonColumn',
-					    'template'=>'{delete}',
-				)
+                array('name'              =>'delete', 
+                	  'type'			  =>'raw', 
+					  'header'            =>false,
+				      'filter'            =>false,
+					  'headerHtmlOptions' => array('width' => '5'),
+					  'htmlOptions'		=>array('style'=>'text-align:left !important'),
+                	  'value' 			  =>'CHtml::link(
+												"<i class=\"icon-remove\"></i>",
+												array(),
+							    				array("data-toggle"=>"tooltip", "data-original-title"=>"Invoice", "class"=>"linkinvoiced",  
+							    					"onclick" => 
+							    					"js:bootbox.confirm(\"Are you sure?\", function(confirmed){
+							    						if(confirmed){
+									    					$.post(\"delete/".$data["id"]."\",{})
+									                            .success(function( data ) {
+									                				$.fn.yiiGridView.update(\"transaction-count-grid\");
+										                            // alert(data );
+										                            // window.location = document.URL;
+									                            });
+															}
+														 })
+													")
+												);',
+                ),
+    //             array(
+				// 	    'class'=>'CButtonColumn',
+				// 	    'template'=>'{delete}',
+				// )
             ),
         )); ?>
 </div>
@@ -86,3 +110,10 @@
 <div class="modal-footer">
 	Fields with <span class="required">*</span> are required.
 </div>
+
+<?php Yii::app()->clientScript->registerScript('verifedIcon', "
+					$('.linkinvoiced').click(function(e){
+                            e.preventDefault();
+                            
+                        });
+                    ", CClientScript::POS_READY); ?>
