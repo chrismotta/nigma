8486a3c80398a3852cdbee6b9e415a5c7f653467
Santiago <santiagomenape@gmail.com>
2015-02-05 15:31:19 -0300

    permisos providers

--- protected/controllers/FinanceController.php
+++ protected/controllers/FinanceController.php
@@ -27,8 +27,12 @@ class FinanceController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('clients','view','excelReport','multiRate','providers','excelReportProviders','sendMail','opportunitieValidation','validateOpportunitie','transaction','addTransaction','invoice','revenueValidation','delete','getCarriers','transactionProviders','deleteTransactionProviders'),
+				'actions'=>array('clients','view','excelReport','multiRate','sendMail','opportunitieValidation','validateOpportunitie','transaction','addTransaction','invoice','revenueValidation','delete','getCarriers'),
 				'roles'=>array('admin', 'finance', 'media','media_manager','businness'),
+			),
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('excelReportProviders','transactionProviders','deleteTransactionProviders','providers'),
+				'roles'=>array('admin', 'finance','media_manager','businness'),
 			),
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('updateValidationStatus'),
