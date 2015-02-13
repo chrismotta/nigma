9f6a3d63960810cf5276aaf7b8a4af5cde58d2b8
hipercubus <subespacios@gmail.com>
2015-02-13 17:12:31 -0300

    fixing tag

--- tag/kickads_tagscript.js
+++ tag/kickads_tagscript.js
@@ -1,5 +1,5 @@
 var bannerLink = document.getElementById('bannerLink');
-bannerLink.innerHTML += '<img src="http://ad.doubleclick.net/ad/N884.1918823KICKADS/B8423623.113859519;sz=1x1;ord=[timestamp]?" border=0 width=1 height=1 alt="Advertisement"></a>';
+bannerLink.innerHTML += '<img src="http://ad.doubleclick.net/ad/N884.1918823KICKADS/B8423623.113859519;sz=1x1;ord=[timestamp]?" border=0 width=1 height=1 alt="Advertisement" />';
 
 if (mraid.getState() == 'loading') {
 	mraid.addEventListener('ready', function(state) {
