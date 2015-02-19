f67255711e2ecaa78e42f7bc75a0ec8bc27f92cd
hipercubus <subespacios@gmail.com>
2015-02-19 15:50:33 -0300

    tag3

---
+++ tag/kickads_tagscript_3.js
@@ -0,0 +1,27 @@
+if (mraid.getState() == 'loading') {
+	mraid.addEventListener('ready', function(state) {
+	    initDefaultState();
+	});
+} else {
+	initDefaultState();
+}
+
+function initDefaultState() {
+    mraid.setExpandProperties({
+	    width : 320,
+	    height : 480,
+	    useCustomClose:true,
+	    isModal:false
+    });
+}
+
+function clickBanner(){
+	mraid.expand("http://www.kickads.mobi/propuestas/ledesma/index.html");
+	console.log("= "+mraid.getState());
+}
+
+function bigClose(){
+	mraid.close();
+	console.log("= "+mraid.getState());
+	return false;
+}
