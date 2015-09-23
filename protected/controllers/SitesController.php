3892c667710f6f72e9c80d8a02605cac5a81efb1
hipercubus <subespacios@gmail.com>
2015-09-23 15:04:52 -0300

    sites fix

--- protected/controllers/SitesController.php
+++ protected/controllers/SitesController.php
@@ -171,7 +171,7 @@ class SitesController extends Controller
 	{
 		// $sizes      = CHtml::listData( BannerSizes::model()->findAll(array('order'=>'width, height')), 'id', 'size' );
 		// $exchanges  = CHtml::listData( Exchanges::model()->findAll(array('order'=>'name')), 'id', 'name');
-		$publishers = CHtml::listData( Publishers::model()->with('providers')->findAll(array('order'=>'providers.name', 'condition' => "providers.status='Active'")), 'providers_id', 'providers.name');
+		$publishers = CHtml::listData( Providers::model()->findAll(array('order'=>'name', 'condition' => "type='Publisher' AND status='Active'")), 'id', 'name');
 		$model_pub = KHtml::enumItem($model, 'model');
 
 		$this->renderPartial('_form', array(
