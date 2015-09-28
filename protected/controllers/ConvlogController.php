0525ebb5e382f52cb5c10d03e03fe153ffba610e
hipercubus <subespacios@gmail.com>
2015-09-28 02:28:40 -0300

    back

--- protected/controllers/ConvlogController.php
+++ protected/controllers/ConvlogController.php
@@ -147,7 +147,7 @@ class ConvlogController extends Controller
 				$conv->campaigns_id = $click->campaigns_id;
 				$conv->clicks_log_id = $click->id;
 				$conv->rate = $conv->campaign->external_rate;
-				$conv->date       = date("Y-M-D G:i:s");
+				// $conv->date       = date("Y-M-D G:i:s");
 
 				$conv->save();
 
