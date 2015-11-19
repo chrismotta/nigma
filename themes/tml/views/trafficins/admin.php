4dd82d9cc2ef88836cef4b02da2a35e4d0f55ead
hipercubus <subespacios@gmail.com>
2015-11-19 13:34:42 -0300

    trafficins admin

--- themes/tml/views/trafficins/admin.php
+++ themes/tml/views/trafficins/admin.php
@@ -43,11 +43,14 @@ return false;
 'dataProvider'=>$model->search(),
 'filter'=>$model,
 'columns'=>array(
-		'id',
-		'tag_id',
-		'server_data',
-array(
-'class'=>'bootstrap.widgets.TbButtonColumn',
-),
+	'id',
+	'tag_id',
+	array(
+		'name'=>'server_data',
+		'htmlOptions'=>array('style'=>'width:900px;word-wrap:break-word;word-break:break-all;'),
+		),
+	// array(
+	// 'class'=>'bootstrap.widgets.TbButtonColumn',
+	// ),
 ),
 )); ?>
