2e2fb08a95a06ccc4fbc3e9de7250876f2bcca05
hipercubus <subespacios@gmail.com>
2015-12-18 12:48:13 -0300

    adv multi modificado opp multi restaurado

--- protected/components/KHtml.php
+++ protected/components/KHtml.php
@@ -523,8 +523,8 @@ class KHtml extends CHtml
         $criteria->order = 't.id, advertisers.name, country.ISO2';
 
 
-        if (FilterManager::model()->isUserTotalAccess('media'))
-            $accountManagerId=Yii::app()->user->id;
+        // if (FilterManager::model()->isUserTotalAccess('media'))
+        //     $accountManagerId=Yii::app()->user->id;
 
         if ( $accountManagerId != NULL )
             $criteria->compare('t.account_manager_id', $accountManagerId);
@@ -727,10 +727,8 @@ class KHtml extends CHtml
             'multiple' => 'multiple',
         );
         $htmlOptions = array_merge($defaultHtmlOptions, $htmlOptions); 
-        $criteria = new CDbCriteria;
-        $criteria->with  = array('regions', 'regions.financeEntities.advertisers','accountManager');
-        $criteria->order = 'advertisers.name';
-
+        
+        /*
 
         if (FilterManager::model()->isUserTotalAccess('media'))
             $accountManager=Yii::app()->user->id;
@@ -757,16 +755,26 @@ class KHtml extends CHtml
         }
 
         $opps = Opportunities::model()->with('regions','regions.financeEntities')->findAll($criteria);
+       
         $data=array();
         foreach ($opps as $opp) {
             $data[$opp->regions->financeEntities->advertisers->id]=$opp->regions->financeEntities->advertisers->name;
         }
+        */
+       
+        $criteria = new CDbCriteria;
+        // $criteria->with  = array('regions', 'regions.financeEntities.advertisers','accountManager');
+        $criteria->order = 't.name';
+
+        $list = Advertisers::model()->findAll($criteria);
+        $data   = CHtml::listData($list, 'id', 'name');
+       
         return Yii::app()->controller->widget(
                 'yiibooster.widgets.TbSelect2',
                 array(
                 'name'        => $name,
                 'data'        => $data,
-                'value'       =>$value,
+                'value'       => $value,
                 'htmlOptions' => $htmlOptions,
                 'options'     => array(
                     'placeholder' => 'All advertisers',
