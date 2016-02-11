d1b7dab605db027a49cd240ca23917851bf3af20
hipercubus <subespacios@gmail.com>
2016-02-11 16:34:37 -0300

    comment

--- themes/tml/views/tag/view.php
+++ themes/tml/views/tag/view.php
@@ -6,6 +6,7 @@
 <?php 
 echo $code; 
 if($tag->analyze){
+	// forensiq pixel
 	echo '<img src="https://www.fqtag.com/pixel.cgi?org=2upravadave3hajasudr&p='.$imp->placements_id.'&a='.$imp->pubid.'&cmp='.$tag->id.'&fmt=banner&rt=displayImg&pfm=Platform&sl=1&fq=1" width="1" height="1" border="0" />';
 }
 ?>
