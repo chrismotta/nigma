fd88221b1d81290e6796f98dec1d4ad23157dcd8
hipercubus <subespacios@gmail.com>
2016-03-01 12:48:35 -0300

    TML prefix

--- themes/tml/views/tag/view.php
+++ themes/tml/views/tag/view.php
@@ -7,7 +7,7 @@
 echo $code; 
 if($tag->analyze){
 	// forensiq pixel
-	echo '<img src="https://www.fqtag.com/pixel.cgi?org=2upravadave3hajasudr&p='.$imp->placements_id.'&a='.$imp->pubid.'&cmp='.$tag->id.'&fmt=banner&rt=displayImg&pfm=Platform&sl=1&fq=1" width="1" height="1" border="0" />';
+	echo '<img src="https://www.fqtag.com/pixel.cgi?org=2upravadave3hajasudr&p=TML_'.$imp->placements_id.'&a='.$imp->pubid.'&cmp=TML_'.$tag->id.'&fmt=banner&rt=displayImg&pfm=Platform&sl=1&fq=1" width="1" height="1" border="0" />';
 }
 ?>
 </body>
