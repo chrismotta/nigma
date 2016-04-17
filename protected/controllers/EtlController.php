74b87eb36fe818b79fcaa5c5fd0428261ed14d8a
hipercubus <subespacios@gmail.com>
2016-04-17 12:37:05 -0300

    fix ip

--- protected/controllers/EtlController.php
+++ protected/controllers/EtlController.php
@@ -33,7 +33,7 @@ class EtlController extends Controller
 		return array(
 			array('allow',
 				'actions'=>$actions,
-				'ips'=>array('54.88.85.63'),
+				'ips'=>array('52.91.118.99'),
 			),
 			array('allow', 
 				'actions'=>$actions,
