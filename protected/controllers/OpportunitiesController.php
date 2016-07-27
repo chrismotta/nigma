f361924427129455f77bbab5585ffb22d6cb7be8
hipercubus <subespacios@gmail.com>
2016-07-27 19:01:31 -0300

    fix

--- protected/controllers/OpportunitiesController.php
+++ protected/controllers/OpportunitiesController.php
@@ -117,7 +117,7 @@ class OpportunitiesController extends Controller
 			$model->carriers_id      = NULL;
 			$model->rate             = NULL;
 			$model->attributes       = $_POST['Opportunities'];
-			if($model->budget == '') $model->budget = new CDbExpression('NULL');
+			$model->budget = new CDbExpression('NULL');
 			$model->versionCreatedBy = Users::model()->findByPk(Yii::app()->user->id)->username;
 			if($model->save())
 				$this->redirect(array('response', 'id'=>$model->id, 'action'=>'updated'));
