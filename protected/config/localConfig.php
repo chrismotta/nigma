2e7304bff6ae5a1ee1d0ef007c4570305151f901
krazydanny <danielspadafora@gmail.com>
2016-09-12 12:40:29 -0300

    correccio localConfig.php

--- protected/config/localConfig.php
+++ protected/config/localConfig.php
@@ -8,12 +8,12 @@
 		const DB_CONNECTION_STRING = 'mysql:host=localhost;dbname=nigma';
 		const DB_EMULATE_PREPARE = true;
 		const DB_USERNAME = 'root';
-		const DB_PASSWORD = 'mil998';
+		const DB_PASSWORD = 'hadita';
 		const DB_CHARSET = 'utf8';
 		const DB_PARAM_LOGGIN = null;
 		const DB_PROFILING = null;
 		const DB_TIMEZONE = \TML_DB_TIMEZONE;
-		const DB_INIT_SQL = array("SET SESSION time_zone = '". \TML_DB_TIMEZONE ."'",);
+
 
 		private $_dbInitSql;
 
