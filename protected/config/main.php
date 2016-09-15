4b5d16c595c384ed9f95151ab7d8b56646137865
hipercubus <subespacios@gmail.com>
2016-09-15 19:02:20 -0300

    corazonada

--- protected/config/main.php
+++ protected/config/main.php
@@ -37,6 +37,27 @@ function mysqlPolicy( $httpHost, $dbTimeZone='+00:00' ){
 						'emails'  =>array(),
 					);
 			break;
+
+		// amazon test
+		case 'dev.tmlbox.co':
+					$mysqlConnect = array(
+						'connectionString' => 'mysql:host=tml.cch7ui9gbr3f.us-east-1.rds.amazonaws.com;dbname=nigma_dev',
+						'emulatePrepare'   => true,
+						'username'         => 'www-data',
+						'password'         => 'th3m3d14l4b',
+						'charset'          => 'utf8',
+						'initSQLs'         => array(
+				           "SET SESSION time_zone = '".$dbTimeZone."'",
+						),
+					);
+
+					$mailLog = array(
+						'class'   =>'CPhpMailerLogRoute',
+						'levels'  =>'',
+						'subject' =>'',
+						'emails'  =>array(),
+					);
+			break;
 		
 		// amazon prod
 		case '52.206.109.37':
@@ -102,27 +123,6 @@ function mysqlPolicy( $httpHost, $dbTimeZone='+00:00' ){
 							'CharSet'    => "UTF-8",
 	                    ),
 	                );*/
-			break;
-
-		// amazon test
-		case 'dev.tmlbox.co':
-					$mysqlConnect = array(
-						'connectionString' => 'mysql:host=tml.cch7ui9gbr3f.us-east-1.rds.amazonaws.com;dbname=nigma_dev',
-						'emulatePrepare'   => true,
-						'username'         => 'www-data',
-						'password'         => 'th3m3d14l4b',
-						'charset'          => 'utf8',
-						'initSQLs'         => array(
-				           "SET SESSION time_zone = '00:00'",
-						),
-					);
-
-					$mailLog = array(
-						'class'   =>'CPhpMailerLogRoute',
-						'levels'  =>'',
-						'subject' =>'',
-						'emails'  =>array(),
-					);
 			break;
 
 		// amazon sourcecode
