872a6497068168e69f05d9ed5f829dff61636719
hipercubus <subespacios@gmail.com>
2016-11-14 16:21:53 -0300

    2034

--- protected/controllers/ClicklogController.php
+++ protected/controllers/ClicklogController.php
@@ -86,7 +86,7 @@ class ClicklogController extends Controller
 		$tmltoken = $_GET['tmltoken'] ? $_GET['tmltoken'] : '';
 
 		// echo 'No creative found';
-		header('Location: http://tmlbox.co/clicklog/tracking/2133?tmltoken='.$tmltoken);
+		header('Location: http://tmlbox.co/clicklog/tracking/2034?tmltoken='.$tmltoken);
 	}
 
 	private static function qsReplace($url, $qs_array){
