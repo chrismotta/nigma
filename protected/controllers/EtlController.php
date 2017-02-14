d0464c1ac54a433cc23c0fa35e2bc6aed1b56837
hipercubus <subespacios@gmail.com>
2017-02-14 12:37:52 -0300

    fix etl bid date

--- protected/controllers/EtlController.php
+++ protected/controllers/EtlController.php
@@ -302,7 +302,7 @@ class EtlController extends Controller
 		echo 'ETL Impressions: '.$return.' rows inserted - Elapsed time: '.$elapsed.' seg.<hr/>';
 	}
 
-	public function actionBid($date=null){
+	public function actionBid($id=1, $date=null){
 
 		$inicialStart = time();
 		$total = 0;
@@ -310,7 +310,7 @@ class EtlController extends Controller
 		if(isset($date))
 			$dateCondition = 'AND DATE(i.date_time) = "'.$date.'"';
 		else
-			$dateCondition = 'AND i.date_time BETWEEN TIMESTAMP( DATE(NOW()) , SUBDATE( MAKETIME(HOUR(NOW()),0,0) , INTERVAL :h HOUR) ) AND TIMESTAMP( DATE(NOW()) , MAKETIME(HOUR(NOW()),0,0) ) ';	
+			$dateCondition = 'AND i.date_time BETWEEN TIMESTAMP( DATE(NOW()) , SUBDATE( MAKETIME(HOUR(NOW()),0,0) , INTERVAL '.$id.' HOUR) ) AND TIMESTAMP( DATE(NOW()) , MAKETIME(HOUR(NOW()),0,0) ) ';	
 
 		$return = Yii::app()->db->createCommand()
 		->select('MAX(freq_cap) AS fc')
