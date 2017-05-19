e6bce747da36af6b2eb2b90815c9ec8426484da0
hipercubus <subespacios@gmail.com>
2017-05-19 18:09:16 -0300

    bid

--- protected/controllers/EtlController.php
+++ protected/controllers/EtlController.php
@@ -346,9 +346,9 @@ class EtlController extends Controller
 		$inicialStart = time();
 
 		if(isset($date))
-			$dateCondition = 'AND DATE(i.date) = "'.date('Y-m-d', strtotime($date)).'" ';
+			$dateCondition = 'AND DATE(i.date_time) = "'.date('Y-m-d', strtotime($date)).'" ';
 		else
-			$dateCondition = 'AND DATE(i.date) = CURDATE()';
+			$dateCondition = 'AND DATE(i.date_time) = CURDATE()';
 
 		$start = time();
 
@@ -357,15 +357,28 @@ class EtlController extends Controller
 		LEFT JOIN D_Supply s      ON(i.D_Supply_id      = s.placement_id) 
 		LEFT JOIN D_UserAgent u   ON(i.D_UserAgent_id   = u.id) 
 		LEFT JOIN D_GeoLocation g ON(i.D_GeoLocation_id = g.id) 
-		SET revenue = d.rate/1000, cost = s.rate/1000
+		SET 
+		i.revenue = CASE
+        WHEN d.freq_cap IS NULL  THEN d.rate/1000 * i.imps
+        WHEN d.freq_cap > i.imps THEN d.rate/1000 * i.imps
+        ELSE d.rate/1000 * d.freq_cap
+        END,
+		i.cost = CASE
+        WHEN d.freq_cap IS NULL  THEN s.rate/1000 * i.imps
+        WHEN d.freq_cap > i.imps THEN s.rate/1000 * i.imps
+        ELSE s.rate/1000 * d.freq_cap
+        END
 		WHERE 
 		(g.connection_type = d.connection_type OR d.connection_type IS NULL OR d.connection_type = "") 
-		AND (g.country         = d.country         OR d.country         IS NULL OR d.country         = "") 
-		AND (u.os_type         = d.os_type         OR d.os_type         IS NULL OR d.os_type         = "") 
-		AND (CONVERT(u.os_version, DECIMAL(5,2)) >= CONVERT(d.os_version, DECIMAL(5,2)) OR d.os_version IS NULL OR d.os_version = "") 
+		AND 
+		(g.country         = d.country         OR d.country         IS NULL OR d.country         = "") 
+		AND 
+		(u.os_type         = d.os_type         OR d.os_type         IS NULL OR d.os_type         = "") 
+		AND 
+		(CONVERT(u.os_version, DECIMAL(5,2)) >= CONVERT(d.os_version, DECIMAL(5,2)) OR d.os_version IS NULL OR d.os_version = "") 
 		';
 		$query.= $dateCondition;
-
+die($query);
 		$return = Yii::app()->db->createCommand($query)->execute();
 
 		$elapsed = time() - $start;
