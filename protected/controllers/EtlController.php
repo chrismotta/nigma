8be4c1d2c4079722f5236cf690b4c48a345497af
hipercubus <subespacios@gmail.com>
2017-05-30 11:13:15 -0300

    md5

--- protected/controllers/EtlController.php
+++ protected/controllers/EtlController.php
@@ -344,7 +344,7 @@ class EtlController extends Controller
 		$start = time();
 
 		$query = 'INSERT IGNORE INTO F_Imp_Compact (id, D_Demand_id, D_Supply_id, date_time, D_UserAgent_id, D_GeoLocation_id, unique_id, pubid, ip_forwarded, referer_url, referer_app, imps, ad_req) 
-		SELECT i.id, i.tags_id, i.placements_id, i.date, u.id, g.id, MD5(CONCAT(i.server_ip,i.user_agent,date(date))), i.pubid, i.ip_forwarded, i.referer, i.app, count(MD5(CONCAT(i.server_ip,i.user_agent,date(date)))), count(MD5(CONCAT(i.server_ip,i.user_agent,date(date)))) 
+		SELECT i.id, i.tags_id, i.placements_id, i.date, u.id, g.id, MD5(CONCAT(i.server_ip,i.user_agent,date(i.date))), i.pubid, i.ip_forwarded, i.referer, i.app, count(MD5(CONCAT(i.server_ip,i.user_agent,date(i.date)))), count(MD5(CONCAT(i.server_ip,i.user_agent,date(i.date)))) 
 		FROM imp_log i 
 		INNER JOIN D_UserAgent u   ON(i.user_agent = u.user_agent) 
 		INNER JOIN D_GeoLocation g ON(i.server_ip  = g.server_ip) ';
@@ -356,7 +356,7 @@ class EtlController extends Controller
 
 		$query .= 'AND i.placements_id IS NOT NULL AND i.tags_id IS NOT NULL AND i.user_agent IS NOT NULL AND i.server_ip IS NOT NULL ';
 
-		$query .= 'GROUP BY MD5(CONCAT(i.server_ip,i.user_agent,date(date))) ';
+		$query .= 'GROUP BY MD5(CONCAT(i.server_ip,i.user_agent,date(i.date))) ';
 
 		$return = Yii::app()->db->createCommand($query)->bindParam('h',$id)->execute();
 
